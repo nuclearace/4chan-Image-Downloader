fb7cd0f1ca3e4385b8dce6c9987a5839bd592e9f
Erik <nuclear.ace@gmail.com>
2015-02-01 08:33:19 -0500

    #2. I didn't know anyone actually used this lel

--- index.js
+++ index.js
@@ -7,119 +7,130 @@ var path = require("path")
 var imageLimiter = new RateLimiter(1, "second")
 
 function Grabber(thread) {
-	var self = this
-	var eightChan = thread.match(/https?\:\/\/8ch\.net\/(.*)\/res\/(\d*)\.html/)
-	if (eightChan) {
-		this.board = eightChan[1]
-		this.thread = eightChan[2]
-		return fs.mkdir(path.join(__dirname, this.board + "_" + this.thread),
-			self.getThreadJSON(this.board, this.thread, true))
-	}
+    var self = this
+    var eightChan = thread.match(/https?\:\/\/8ch\.net\/(.*)\/res\/(\d*)\.html/)
+    if (eightChan) {
+        this.board = eightChan[1]
+        this.thread = eightChan[2]
+        return fs.mkdir(path.join(__dirname, this.board + "_" + this.thread),
+            self.getThreadJSON(this.board, this.thread, true))
+    }
 
-	var threadInfo = thread.match(/https?\:\/\/boards\.4chan\.org\/(.*)\/thread\/(\d*)/)
-	if (!threadInfo)
-		return console.log("failed")
+    var threadInfo = thread.match(/https?\:\/\/boards\.4chan\.org\/(.*)\/thread\/(\d*)/)
+    if (!threadInfo)
+        return console.log("failed")
 
-	this.board = threadInfo[1]
-	this.thread = threadInfo[2]
+    this.board = threadInfo[1]
+    this.thread = threadInfo[2]
 
-	fs.mkdir(path.join(__dirname, this.board + "_" + this.thread), self.getThreadJSON(this.board, this.thread))
+    fs.mkdir(path.join(__dirname, this.board + "_" + this.thread), self.getThreadJSON(this.board, this.thread))
 }
 
 Grabber.prototype.getImages = function(json, eightChan) {
-	var self = this
-	var threadJSON = JSON.parse(json)
-	var posts = threadJSON["posts"]
+    var self = this
+    var threadJSON = JSON.parse(json)
+    var posts = threadJSON["posts"]
+    var tim, ext, filename, extras
 
-	var createFun = function(filename, ext, tim) {
-		fs.exists(self.board + "_" + self.thread + "/" + tim + ext, function(exists) {
-			if (exists)
-				return console.log("Already exists: " + tim + ext)
-			else
-				postCheck()
-		})
+    var createFun = function(filename, ext, tim) {
+        fs.exists(self.board + "_" + self.thread + "/" + tim + ext, function(exists) {
+            if (exists)
+                return console.log("Already exists: " + tim + ext)
+            else
+                postCheck()
+        })
 
-		var postCheck = function() {
-			var options = {
-				host: "i.4cdn.org",
-				path: "/" + self.board + "/" + tim + ext
-			}
+        var postCheck = function() {
+            var options = {
+                host: "i.4cdn.org",
+                path: "/" + self.board + "/" + tim + ext
+            }
 
-			if (eightChan) {
-				options = {
-					host: "8ch.net",
-					path: "/" + self.board + "/src/" + tim + ext
-				}
-			}
+            if (eightChan) {
+                options = {
+                    host: "8ch.net",
+                    path: "/" + self.board + "/src/" + tim + ext
+                }
+            }
 
-			return imageLimiter.removeTokens(1, function() {
-				self.urlRetrieve(https, options, function(code, buffer) {
-					fs.writeFile(self.board + "_" + self.thread + "/" + tim + ext, buffer, 'binary', function(err) {
-						if (err)
-							console.log(err)
-						else
-							console.log("Got: " + tim + ext)
-					})
-				})
-			})
-		}
-	}
+            return imageLimiter.removeTokens(1, function() {
+                self.urlRetrieve(https, options, function(code, buffer) {
+                    fs.writeFile(self.board + "_" + self.thread + "/" + tim + ext, buffer, 'binary', function(err) {
+                        if (err)
+                            console.log(err)
+                        else
+                            console.log("Got: " + tim + ext)
+                    })
+                })
+            })
+        }
+    }
 
-	for (var i = 0; i < posts.length; i++) {
-		if ("filename" in posts[i]) {
-			var filename = posts[i]["filename"]
-			var ext = posts[i]["ext"]
-			var tim = posts[i]["tim"]
-			createFun(filename, ext, tim)
-		}
-	}
+    for (var i = 0; i < posts.length; i++) {
+        if ("filename" in posts[i]) {
+            filename = posts[i]["filename"]
+            ext = posts[i]["ext"]
+            tim = posts[i]["tim"]
+            createFun(filename, ext, tim)
+
+            extras = posts[i]["extra_files"]
+            if (extras !== undefined) {
+                for (var j = 0; j < extras.length; j++) {
+                    filename = extras[j]["filename"]
+                    ext = extras[j]["ext"]
+                    tim = extras[j]["tim"]
+                    createFun(filename, ext, tim)
+                }
+            }
+        }
+    }
 };
 
 Grabber.prototype.getThreadJSON = function(board, thread, eightChan) {
-	var self = this
-	var options = {
-		host: "a.4cdn.org",
-		path: "/" + board + "/thread/" + thread + ".json"
-	}
+    var self = this
+    var options = {
+        host: "a.4cdn.org",
+        path: "/" + board + "/thread/" + thread + ".json"
+    }
 
-	if (eightChan) {
-		options = {
-			host: "8ch.net",
-			path: "/" + board + "/res/" + thread + ".json"
-		}
-	}
+    if (eightChan) {
+        options = {
+            host: "8ch.net",
+            path: "/" + board + "/res/" + thread + ".json"
+        }
+    }
 
-	imageLimiter.removeTokens(1, function() {
-		self.urlRetrieve(https, options, function(code, json) {
-			if (json)
-				self.getImages(json, eightChan)
-		})
-	})
+    imageLimiter.removeTokens(1, function() {
+        self.urlRetrieve(https, options, function(code, json) {
+            if (json)
+                self.getImages(json, eightChan)
+        })
+    })
 };
 
 Grabber.prototype.urlRetrieve = function(transport, options, callback) {
-	var dom = domain.create()
-	console.log("Get: " + options.host + options.path)
-	dom.on("error", function(err) {
-		console.log("Error")
-	})
-	dom.run(function() {
-		var req = transport.request(options, function(res) {
-			res.setEncoding("binary")
-			var buffer = ""
-			res.on("data", function(chunk) {
-				buffer += chunk
-			})
-			res.on("end", function() {
-				callback(res.statusCode, buffer)
-			})
-		})
-		req.end()
-	})
+    var dom = domain.create()
+    console.log("Get: " + options.host + options.path)
+    dom.on("error", function(err) {
+        console.log("Error")
+    })
+    dom.run(function() {
+        var req = transport.request(options, function(res) {
+            res.setEncoding("binary")
+            var buffer = ""
+            res.on("data", function(chunk) {
+                buffer += chunk
+            })
+            res.on("end", function() {
+                callback(res.statusCode, buffer)
+            })
+        })
+        req.end()
+    })
 };
 
 process.argv.forEach(function(val, index, array) {
-	if (index < 2)
-		return
-	new Grabber(val)
+    if (index < 2)
+        return
+    new Grabber(val)
 })
